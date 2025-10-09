9fa6fbc4d29ebec2265759dc5846ef130696bc96
heloworldz <dundead17@gmail.com>
2025-10-09 12:45:56 +0530

    Update PostsAdapter.java

--- app/src/main/java/com/example/lostfound/PostsAdapter.java
+++ app/src/main/java/com/example/lostfound/PostsAdapter.java
@@ -1,6 +1,6 @@
 package com.lostandfound;
 
-import com.example.lostfoundnetwork.R;
+import com.lostandfound.R;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -64,5 +64,6 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.PostViewHold
     }
 
 }
+
 
 
